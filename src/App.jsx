6c0b0a2bedf9f2ab9b01631bc0a2028ac3be11e5
Vishvendra Tomar <vishvendratomar29@gmail.com>
2024-04-22 11:22:05 +0530

    created canvas

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import CanvasComponent from './components/CanvasComponent'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className='text-2xl pb-5 font-bold text-red-800'>Canvas</h1>
+      <CanvasComponent/>
+    </>
+  )
+}
+
+export default App
